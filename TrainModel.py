6dc192bc333d5e7f262f5bdfc10d9974a5675f8f
An Ha <zero579911@gmail.com>
2021-04-22 05:24:52 -0400

    Use tf.data to load images instead of keras.preprocessing
    The keras.preprocessing utilities is hard to work with and getting the
    total number of images from the dataset is impossible. Using
    tf.data.Dataset.list_files instead makes it easier. We also have more
    control over how the images are processed through this method.

--- TrainModel.py
+++ TrainModel.py
@@ -1,5 +1,7 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import os
 
 from tensorflow.keras import layers, models
 from pathlib import Path
@@ -14,27 +16,76 @@ IMG_DEPTH = 3
 
 data_dir = Path(DATASET_PATH)
 
-full_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    image_size=(IMG_WIDTH, IMG_HEIGHT),
-    batch_size=BATCH_SIZE
+list_ds = tf.data.Dataset.list_files(str(data_dir / "*/*"), shuffle=False)
+DATASET_SIZE = list_ds.cardinality().numpy() # Returns the total number of images found in the dataset folder
+
+class_names = np.array(
+    sorted([item.name for item in data_dir.glob("*") if item.name != "LICENSE.txt"])
 )
-DATASET_SIZE = full_ds.cardinality().numpy() # Note this returns TOTAL_NUM_IMAGES / BATCH_SIZE
-NUM_CLASSES = len(full_ds.class_names)
+NUM_CLASSES = len(class_names)
 
 TRAIN_SIZE = int(0.70 * DATASET_SIZE)
 VAL_SIZE = int(0.15 * DATASET_SIZE)
 TEST_SIZE = int(0.15 * DATASET_SIZE)
 
-full_ds = full_ds.shuffle(DATASET_SIZE)
-train_ds = full_ds.take(TRAIN_SIZE)
-test_ds = full_ds.skip(TRAIN_SIZE)
+list_ds = list_ds.shuffle(DATASET_SIZE, reshuffle_each_iteration=False)
+train_ds = list_ds.take(TRAIN_SIZE)
+test_ds = list_ds.skip(TRAIN_SIZE)
 val_ds = test_ds.take(VAL_SIZE)
 test_ds = test_ds.skip(VAL_SIZE)
 
-train_ds = train_ds.cache().shuffle(TRAIN_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
-val_ds = val_ds.cache().shuffle(VAL_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
-test_ds = test_ds.cache().prefetch(tf.data.experimental.AUTOTUNE)
+
+def get_label(file_path):
+    # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # The second to last is the class-directory
+    one_hot = parts[-2] == class_names
+    # Integer encode the label
+    return tf.argmax(one_hot)
+
+
+def decode_img(img):
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # resize the image to the desired size
+    return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
+
+
+def process_path(file_path):
+    label = get_label(file_path)
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
+
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+train_ds = train_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+test_ds = test_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+
+def configure_for_performance(ds, is_test_dataset=False):
+    # Testing pipeline is similar to training pipeline, except that
+    # 1. There is no ds.shuffle() call
+    # 2. Caching is done after batching (as batches can be the same between epoch)
+    # Reference: https://www.tensorflow.org/datasets/keras_example
+    if is_test_dataset:
+        ds = ds.batch(BATCH_SIZE)
+        ds = ds.cache()
+        ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    else:
+        ds_size = ds.cardinality().numpy()
+        ds = ds.cache()
+        ds = ds.shuffle(buffer_size=ds_size)
+        ds = ds.batch(BATCH_SIZE)
+        ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    return ds
+
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+test_ds = configure_for_performance(test_ds, is_test_dataset=True)
 
 model = models.Sequential()
 model.add(layers.experimental.preprocessing.Rescaling(1./255, input_shape=(IMG_WIDTH, IMG_HEIGHT, IMG_DEPTH)))
