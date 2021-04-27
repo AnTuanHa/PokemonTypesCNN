9c0c20fec73e034439721aae197d36768a73a50a
An Ha <zero579911@gmail.com>
2021-04-27 04:38:20 -0400

    deploy

---
+++ precache-manifest.51c5235918c2faaa2788a1d8fe8fe593.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd6986e0cdce07fba8d",
+    "url": "/PokemonTypesCNN/css/app.8849d0cd.css"
+  },
+  {
+    "revision": "388e007a380de6ab22fbc8a43594dc0e",
+    "url": "/PokemonTypesCNN/index.html"
+  },
+  {
+    "revision": "dcd6986e0cdce07fba8d",
+    "url": "/PokemonTypesCNN/js/app.c8982635.js"
+  },
+  {
+    "revision": "a23aec585076fd2cf68d",
+    "url": "/PokemonTypesCNN/js/chunk-vendors.0737a3e7.js"
+  },
+  {
+    "revision": "e3d2b7068101da3882ae5ba5ac58828b",
+    "url": "/PokemonTypesCNN/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PokemonTypesCNN/robots.txt"
+  },
+  {
+    "revision": "2d36e748ee60696e4fc3f8f34e0ef76b",
+    "url": "/PokemonTypesCNN/trained_model/group1-shard1of1.bin"
+  },
+  {
+    "revision": "53a4166c7e8a5abda6b8836e9a56d4e1",
+    "url": "/PokemonTypesCNN/trained_model/model.json"
+  }
+]);
